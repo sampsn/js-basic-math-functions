691d9bf32db2a038acaa68a621ef3ee1d7ac1816
sampsn <g@samp.sn>
2024-06-04 11:46:02 -0600

    fix functions

--- js/script.js
+++ js/script.js
@@ -1,43 +1,47 @@
 const add = (x, y) => {
-  return x + y;
+	return x + y;
 };
 
 const mul = (x, y) => {
-  return x * y;
+	return x * y;
 };
 
 const div = (x, y) => {
-  return x / y;
+	return x / y;
+};
+
+const square = (x) => {
+	return multiply(x, x);
 };
 
 const addSquares = (x, y) => {
-  return x * x + y * y;
+	return add(square(x), square(y));
 };
 
 document.getElementById("add").addEventListener("click", () => {
-  document.getElementById("result").innerHTML = add(
-    Number(document.getElementById("first").value),
-    Number(document.getElementById("second").value),
-  );
+	document.getElementById("result").innerHTML = add(
+		Number(document.getElementById("first").value),
+		Number(document.getElementById("second").value),
+	);
 });
 
 document.getElementById("mul").addEventListener("click", () => {
-  document.getElementById("result").innerHTML = mul(
-    Number(document.getElementById("first").value),
-    Number(document.getElementById("second").value),
-  );
+	document.getElementById("result").innerHTML = mul(
+		Number(document.getElementById("first").value),
+		Number(document.getElementById("second").value),
+	);
 });
 
 document.getElementById("div").addEventListener("click", () => {
-  document.getElementById("result").innerHTML = div(
-    Number(document.getElementById("first").value),
-    Number(document.getElementById("second").value),
-  );
+	document.getElementById("result").innerHTML = div(
+		Number(document.getElementById("first").value),
+		Number(document.getElementById("second").value),
+	);
 });
 
 document.getElementById("add-squares").addEventListener("click", () => {
-  document.getElementById("result").innerHTML = addSquares(
-    Number(document.getElementById("first").value),
-    Number(document.getElementById("second").value),
-  );
+	document.getElementById("result").innerHTML = addSquares(
+		Number(document.getElementById("first").value),
+		Number(document.getElementById("second").value),
+	);
 });
